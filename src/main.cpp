f7388b43195c4dff4dda4ec6dcf569fc9e6e17e1
Aart <aart_odding@hotmail.com>
2020-04-06 21:03:25 +0200

    got most dependencies setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
 
+#include <ZigZag/Object.hpp>
+
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+ 
+static void error_callback(int error, const char* description)
+{
+    std::cout << "Error:\n" << description << std::endl;
+}
+ 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 
 int main()
 {
+    GLFWwindow* window;
+ 
+    glfwSetErrorCallback(error_callback);
+ 
+ 
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+ 
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        throw "error";
+    }
+ 
+    glfwSetKeyCallback(window, key_callback);
+ 
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+ 
+  
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+ 
+    glfwDestroyWindow(window);
+ 
+    glfwTerminate();
+
+    ZigZag::Object obj(nullptr, "hay");
     std::cout << "hello" << std::endl;
 
     return 0;
