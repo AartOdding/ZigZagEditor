48e083d02b9f6f3969f140a4499df9518e942a31
Aart <aart_odding@hotmail.com>
2020-07-11 23:00:59 +0200

    Started working on creating connections

--- src/gui/Viewport.cpp
+++ src/gui/Viewport.cpp
@@ -1,7 +1,7 @@
 #include "Viewport.hpp"
 
 
-
+#include <iostream>
 
 Viewport::Viewport()
 {
@@ -30,7 +30,14 @@ void Viewport::draw(bool* open)
 
     ImNode::SetCurrentEditor(m_editorContext);
     ImNode::Begin("Graph Editor");
-    ImNode::PushStyleVar(ImNode::StyleVar_NodePadding, 0);
+
+    ImNode::PushStyleVar(ImNode::StyleVar_NodePadding, {0, 0, 0, 0});
+    ImNode::PushStyleVar(ImNode::StyleVar_NodeRounding, 3);
+    ImNode::PushStyleVar(ImNode::StyleVar_NodeBorderWidth, 0);
+    ImNode::PushStyleVar(ImNode::StyleVar_HoveredNodeBorderWidth, 0);
+    ImNode::PushStyleVar(ImNode::StyleVar_SelectedNodeBorderWidth, 0);
+
+    //ImNode::PushStyleColor(ImNode::StyleColor_NodeBg, { 1, 1, 1, 0 });
 
     if (m_scope)
     {
@@ -39,7 +46,7 @@ void Viewport::draw(bool* open)
             ImNode::BeginNode((uint64_t)op);
             auto localPos = ImGui::GetCursorPos();
             ImGui::Dummy({ 100, 100 });
-            ImGui::SetCursorPos(localPos);
+            ImGui::SetCursorPos({ localPos.x + 10, localPos.y + 10 });
             ImGui::Text(op->getName().c_str());
 
             auto& inputs = op->getDataInputs();
@@ -64,33 +71,52 @@ void Viewport::draw(bool* open)
                 ImGui::Dummy({ 20, 20 });
                 ImNode::EndPin();
             }
-
-            //for (int i = 0; i < max; ++i)
-            //{
-            //    if (i < inputs.size())
-            //    {
-            //        ImNode::BeginPin((uint64_t)inputs[i], ImNode::PinKind::Input);
-            //        ImGui::Text(inputs[i]->getName().c_str());
-            //        ImNode::EndPin();
-            //    }
-            //    else
-            //    {
-            //        ImGui::Text(" ");
-            //    }
-            //    ImGui::SameLine();
-
-            //    if (i < outputs.size())
-            //    {
-            //        ImNode::BeginPin((uint64_t)outputs[i], ImNode::PinKind::Output);
-            //        ImGui::Text(outputs[i]->getName().c_str());
-            //        ImNode::EndPin();
-            //    }
-            //}
             ImNode::EndNode();
+        }
+        for (auto op : m_scope->getChildOperators())
+        {
+            for (auto output : op->getDataSources())
+            {
+                for (auto input : output->getConnectedInputs())
+                {
+                    // use input for link id because input can only have one conection.
+                    ImNode::Link((uint64_t)input, (uint64_t)output, (uint64_t)input);
+                }
+            }
         }
     }
 
-    ImNode::PopStyleVar(1);
+    if (ImNode::BeginCreate())
+    {
+        ImNode::PinId inputId, outputId;
+
+        if (ImNode::QueryNewLink(&inputId, &outputId))
+        {
+            static ZigZag::BaseDataInput* prevIn = nullptr;
+            static ZigZag::BaseDataSource* prevOut = nullptr;
+
+            auto input = inputId.AsPointer<ZigZag::BaseDataInput>();
+            auto output = outputId.AsPointer<ZigZag::BaseDataSource>();
+
+            if (input != prevIn || output != prevOut)
+            {
+                prevIn = input;
+                prevOut = output;
+                std::cout << input << "\t" << output << std::endl;
+            }
+
+            // AcceptNewItem returns true if the mouse is released
+            if (ImNode::AcceptNewItem())
+            {
+                std::cout << "connected " << input << "\t" << output << std::endl;
+                connect(output, input);
+            }
+        }
+    }
+    ImNode::EndCreate();
+
+    //ImNode::PopStyleColor(1);
+    ImNode::PopStyleVar(5);
     ImNode::End();
 
     ImGui::End();
