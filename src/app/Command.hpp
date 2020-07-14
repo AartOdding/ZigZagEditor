3187da9bd64b3fb43a538e5d1ba186857f14865f
Aart <aart_odding@hotmail.com>
2020-07-14 22:43:00 +0200

    removed sub-commands

--- src/app/Command.hpp
+++ src/app/Command.hpp
@@ -7,8 +7,6 @@
 
 class Command
 {
-	friend class CommandStack;
-
 public:
 
 	virtual ~Command() = default;
@@ -16,19 +14,7 @@ public:
 	virtual const std::string& typeName() = 0;
 	virtual const std::string& description() = 0;
 
-protected:
-
 	virtual void redo() = 0;
 	virtual void undo() = 0;
-
-	template <typename T, typename ... Args>
-	void addSubCommand(Args&& ... args)
-	{
-		m_subCommands.push_back(std::make_unique<T>(std::forward<Args>(args) ...));
-	}
-
-private:
-
-	std::vector<std::unique_ptr<Command>> m_subCommands;
 
 };
